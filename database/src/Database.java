28990ea00badaf21a396cd6ffb36c99eaf427a2f
Finn Holme <finn@Finns-MacBook-Pro.local>
2016-03-11 15:58:03 +0100

    Database.java now uses scriptrunner for creating the database

--- database/src/Database.java
+++ database/src/Database.java
@@ -1,15 +1,36 @@
+import java.io.*;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Database {
     // JDBC driver name and database URL
+    static final String deiteibeise= "";
+
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     // Autoreconnect=true is very convenient while useSSL=false is often necessary
-    static final String DB_URL = "jdbc:mysql://localhost:3306/?autoReconnect=true&useSSL=false";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/db?autoReconnect=true&useSSL=false";
 
     //  Database credentials
     //  My firewall stops you from logging in
     static final String USER = "user";
     static final String PASS = "hunter2";
+    static final String filePath = "src/createdb.sql";
+
+    private static String statementLoader(String filePath){
+        try{File f = new File(filePath);
+        FileInputStream fin = new FileInputStream(f);
+        byte[] buffer = new byte[(int) f.length()];
+        new DataInputStream(fin).readFully(buffer);
+        fin.close();
+        String s = new String(buffer, "ASCII");
+        return s;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return "";
+        }
+
+    }
 
     public static void main(String[] args) {
         Connection conn = null;
@@ -22,13 +43,9 @@ public class Database {
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            // Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            //TODO: Initialize with our sql script.
-            String sql = "CREATE DATABASE STUDENTS";
-            stmt.executeUpdate(sql);
+            ScriptRunner runner = new ScriptRunner(conn, false, false);
+            String file = filePath;
+            runner.runScript(new BufferedReader(new FileReader(file)));
             System.out.println("Database created successfully...");
         }catch(SQLException se){
             //Handle errors for JDBC
